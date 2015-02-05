da93ecdefa262ad1da4f2a1f91e298b2557e9326
RYO NAKAE <me@ryonakae.com>
2015-02-05 23:02:37 +0900

    アップデート用のスクリプトが動かなかったので修正

--- cask_upgrade.sh
+++ cask_upgrade.sh
@@ -1,6 +1,15 @@
+caskroom="/usr/local/Caskroom"
 apps=($(brew cask list))
 for a in ${apps[@]};do
-  if grep -q "Not installed" < <(brew cask info $a);then
+  info=$(brew cask info $a)
+  if echo "$info"| grep -q "Not installed";then
     brew cask install $a
   fi
+  current=$(echo "$info"|grep "${caskroom}/${a}"|cut -d' ' -f1)
+  for dir in $(ls ${caskroom}/${a});do
+    testdir="${caskroom}/${a}/${dir}"
+    if [ "$testdir" != "$current" ];then
+      rm -rf "$testdir"
+    fi
+  done
 done
