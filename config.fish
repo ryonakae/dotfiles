b109967b53f61275757e59fb37380fc379eebbba
Ryo Nakae <ryo.n.1129@gmail.com>
2019-10-01 20:56:04 +0900

    update fish config

--- config.fish
+++ config.fish
@@ -1,3 +1,4 @@
+# theme
 set -g theme_display_date no
 set -g theme_powerline_fonts no
 set -g theme_nerd_fonts yes
@@ -7,9 +8,19 @@ set -g theme_color_scheme dracula
 set -g theme_display_user ssh
 set -g theme_display_hostname ssh
 
+# fzf
 set -U FZF_LEGACY_KEYBINDINGS 0
 
+# env
 status --is-interactive; and source (nodenv init -|psub)
 status --is-interactive; and source (rbenv init -|psub)
 status --is-interactive; and source (pyenv init -|psub)
 status --is-interactive; and source (goenv init -|psub)
+
+# go
+set -Ux GOPATH $HOME/go
+set -x PATH $GOPATH/bin $PATH
+
+# google-cloud sdk
+set -x PATH /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/platform/google_appengine $PATH
+source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
