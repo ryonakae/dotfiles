825ff10397fd786074483817461885b740680608
Ryo Nakae <ryo.n.1129@gmail.com>
2019-10-01 21:10:45 +0900

    update fish oath

--- config.fish
+++ config.fish
@@ -11,11 +11,23 @@ set -g theme_display_hostname ssh
 # fzf
 set -U FZF_LEGACY_KEYBINDINGS 0
 
-# env
+# homebrew
+set -x HOMEBREW_CASK_OPTS --appdir=/Applications
+
+# *env
 status --is-interactive; and source (nodenv init -|psub)
 status --is-interactive; and source (rbenv init -|psub)
 status --is-interactive; and source (pyenv init -|psub)
 status --is-interactive; and source (goenv init -|psub)
+
+# direnv
+set -x EDITOR Vim
+
+# openssl
+set -x PATH /usr/local/opt/openssl/bin $PATH
+set -x LDFLAGS -L/usr/local/opt/openssl/lib
+set -x CPPFLAGS -I/usr/local/opt/openssl/include
+set -x PKG_CONFIG_PATH /usr/local/opt/openssl/lib/pkgconfig
 
 # go
 set -Ux GOPATH $HOME/go
@@ -24,3 +36,6 @@ set -x PATH $GOPATH/bin $PATH
 # google-cloud sdk
 set -x PATH /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/platform/google_appengine $PATH
 source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
+
+# Android SDK
+set -x PATH $HOME/Library/Android/sdk/platform-tools $PATH
