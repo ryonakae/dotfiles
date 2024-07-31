61d6328c16abfe09607a80bc9c86026b76d2d901
Ryo Nakae <ryo.n.1129@gmail.com>
2024-07-31 17:07:24 +0900

    update files location

--- symlink.sh
+++ symlink.sh
@@ -1,21 +1,34 @@
-DOT_FILES=(.zshrc .vimrc .hyper.js .huskyrc)
+# $HOME直下に置くファイル
+DOT_FILES=(.zshrc .vimrc .hyper.js)
 for file in ${DOT_FILES[@]}
 do
   if [ -a $HOME/$file ]; then
-      echo "$file が存在するのでシンボリックリンクを貼りませんでした"
+    echo "$file が存在するのでシンボリックリンクを貼りませんでした"
   else
     ln -s $HOME/dotfiles/$file $HOME/$file
-      echo "$file のシンボリックリンクを貼りました"
+    echo "$file のシンボリックリンクを貼りました"
   fi
 done
 
-FISH_FILES=(config.fish fish_plugins)
+# fishの設定ファイル
+mkdir -p $HOME/.config/fish
+FISH_FILES=(.config/fish/config.fish .config/fish/fish_plugins)
 for file in ${FISH_FILES[@]}
 do
-  if [ -a $HOME/.config/fish/$file ]; then
-      echo "$file が存在するのでシンボリックリンクを貼りませんでした"
+  if [ -a $HOME/$file ]; then
+    echo "$file が存在するのでシンボリックリンクを貼りませんでした"
   else
-    ln -s $HOME/dotfiles/$file $HOME/.config/fish/$file
-      echo "$file のシンボリックリンクを貼りました"
+    ln -s $HOME/dotfiles/$file $HOME/$file
+    echo "$file のシンボリックリンクを貼りました"
   fi
 done
+
+# huskyの設定ファイル
+mkdir -p $HOME/.config/husky
+HUSKY_FILE=.config/husky/init.sh
+if [ -a $HOME/$HUSKY_FILE ]; then
+  echo "$HUSKY_FILE が存在するのでシンボリックリンクを貼りませんでした"
+else
+  ln -s $HOME/dotfiles/$HUSKY_FILE $HOME/$HUSKY_FILE
+  echo "$HUSKY_FILE のシンボリックリンクを貼りました"
+fi
